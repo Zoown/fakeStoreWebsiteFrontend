86e52375899ced3d2c414d2a170116f017f5a290
Alexander Borggren <borggren4@gmail.com>
2024-03-26 22:40:39 +0800

    fix cart styling

--- js/scriptsCart.js
+++ js/scriptsCart.js
@@ -5,23 +5,53 @@ window.onload = function() {
     // Nu kan du använda 'selectedProduct' på din form-sida
     console.log(selectedProduct);
 
+    //<p>Total <span class="price" style="color:black"><b>$30</b></span></p>
     // Skapa nya element för att visa produktinformation
     let linkA = document.createElement('a');
+    let productImage = document.createElement('img');
     let title = document.createElement('p');
     let price = document.createElement('span');
+    let totalPrice = document.createElement('span');
+    let totalPriceTitle = document.createElement('p');
+    let totalPriceHead = document.createElement('hr');
+    let boldTotalPrice = document.createElement('b');
 
-    // title.setAttribute("class", "product-title");
+    totalPriceTitle.style.fontWeight = "bold";
+    totalPrice.className = 'price';
+    totalPrice.style.color = "black";
+    totalPriceTitle.style.fontSize = "22px";
+    totalPriceTitle.style.marginBottom = "-5px";
+    totalPrice.style.marginBottom = "-10px";
+    totalPrice.style.fontSize = "16px";
+    title.style.marginLeft = "20px";
+    title.style.fontWeight = "bold";
+    title.style.fontSize = "14px";
     price.setAttribute("class", "price");
     linkA.setAttribute("href", "#");
+    productImage.setAttribute("src", selectedProduct.image,);
+    productImage.style.height = '30px';
+    productImage.style.width = '30px'; // inline css
+    productImage.style.objectFit = 'cover';
+    productImage.style.float = 'left';
+    productImage.style.marginRight = "10px";
+
+
     //price.setAttribute("")
 
     // Ange textinnehållet för de nya elementen
-    linkA.textContent = "Product 1123123123123";
+    title.textContent = selectedProduct.title;
     price.textContent = '$' + selectedProduct.price;
+    totalPriceTitle.textContent = 'Total';
+    boldTotalPrice.textContent = '$' + selectedProduct.price;
 
     // Lägg till de nya elementen i dokumentet
     let row = document.querySelector('#start-list');
+    row.appendChild(productImage);
     row.appendChild(title);
     title.appendChild(linkA);
     title.appendChild(price);
+    row.appendChild(totalPriceHead);
+    row.appendChild(totalPriceTitle);
+    totalPrice.appendChild(boldTotalPrice);
+    totalPriceTitle.appendChild(totalPrice);
 };
